7bb9f5aeeadf4c3ec2801a4e79be0c4bf778af0e
pdrlcscrdr <166539206+pdrlcscrdr@users.noreply.github.com>
2024-04-12 16:32:17 -0300

    Formas de exibir na tela.

---
+++ l5q1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+//Formas de exibição (Hello, World!)
+int main(){
+	printf("Hello, World!\n");
+	printf("Hello,\nWorld!\n");
+	printf("\tHello, World!\n");
+	printf("Hello,\n\tWorld!");
+	return(0);
+}
