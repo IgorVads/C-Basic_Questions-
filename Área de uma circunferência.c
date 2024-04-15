103ebcc5000fb77daf1c1618f2b824da057c10aa
Pedro <166539206+pedrolucascrdr@users.noreply.github.com>
2024-04-15 15:50:36 -0300

    Área de uma circunferência.c

--- Área de uma circunferência.c
+++ Área de uma circunferência.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 #include<locale.h>
 #include<math.h>
-//Cálculo da área de uma circunferência
+//CÃ¡lculo da Ã¡rea de uma circunferÃªncia
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	float a, pi, r;
 	pi = 3,14;
-	printf("Informe o raio da circunferência: \n");
+	printf("Informe o raio da circunferÃªncia: \n");
 	scanf("%f", &r);
 	a = pi * pow(r, 2);
-	printf("O valor da área é: %.1f. \n", a);
+	printf("O valor da Ã¡rea Ã©: %.1f. \n", a);
 	return(0);
 }
