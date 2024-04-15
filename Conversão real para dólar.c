d10e128a4a39cce94760cbdc6d952142ea54042e
Pedro <166539206+pedrolucascrdr@users.noreply.github.com>
2024-04-15 15:54:02 -0300

    Conversão real para dólar.c

--- Conversão real para dólar.c
+++ Conversão real para dólar.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #include<locale.h>
-//Convers„o real para dÛlar
+//Convers√£o real para d√≥lar
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	float d, r, c;
 	d = 5;
-	printf("O valor do dÛlar atualmente È $%.1f. \n", d);
+	printf("O valor do d√≥lar atualmente √© $%.1f. \n", d);
 	printf("Informe o valor em real para ser convertido: \n");
 	scanf("%f", &r);
 	c = r * d;
-	printf("O resultado da convers„o foi: $%.1f. \n", c);
+	printf("O resultado da convers√£o foi: $%.1f. \n", c);
 	return(0);
 }
 	
