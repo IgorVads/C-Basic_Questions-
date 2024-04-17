fa2bcea0cde016fedb55a67f352e9875b54836c8
Igor Vinicius <161034058+IgorVads@users.noreply.github.com>
2024-04-17 16:04:17 -0300

    Update Cálculo do IMC.c
    Adicionado comentário mais explicativo do objetivo do programa e alteração na sintaxe do programa para uma melhor compreensão do código.

--- Cálculo do IMC.c
+++ Cálculo do IMC.c
@@ -1,16 +1,25 @@
 #include<stdio.h>
 #include<locale.h>
 #include<math.h>
-//Cálculo do IMC de uma pessoa
+
+/*Um programa que solicita e lê o peso e a altura de uma pessoa e apresenta seu IMC com duas casas decimais 
+após a vírgula. O IMC é calculado por meio da seguinte fórmula: IMC = PESO/ALTURA^2 */
+
 int main(){
+	
 	setlocale(LC_ALL, "Portuguese");
+	
 	float imc, a, p;
+	
 	//Solicitando o peso e a altura do usuário
 	printf("Informe seu peso: \n");
-	scanf("%f", &p);
+		scanf("%f", &p);
+	
 	printf("Informe sua altura: \n");
-	scanf("%f", &a);
+		scanf("%f", &a);
+	
 	imc = p / pow(a , 2);
+	
 	printf("Seu IMC é %.1f. \n", imc);
 	return(0);
 }
